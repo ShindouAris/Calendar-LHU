11554d3c26428ea8e44a703507589a98ff1e7472
ArisDEV <tranthanhnam.ccc@gmail.com>
2025-09-04 22:14:22 +0700

    thêm analytics của vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { registerSW } from 'virtual:pwa-register';
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
 
